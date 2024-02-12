a6d5802ccfdaf892f84a17e06fe2b58aaf4be067
Christoph Pasterniak <cpasterniak@student.tgm.ac.at>
2024-02-12 21:28:59 +0100

    Programm works

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,17 @@ public class Main {
             s.push(Integer.valueOf(1));
             s.push(Integer.valueOf(2));
             s.push(Integer.valueOf(3));
-            s.pop();
-            s.push(Integer.valueOf(3));
         }catch(StackFullException e) {
             System.out.println(e);
         }
+        try {
+            s.pop();
+            s.pop();
+            s.pop();
+        } catch(StackEmptyException e) {
+            System.out.println(e);
+        }
+
         System.out.println(s.peek());
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -2,24 +2,34 @@ import java.util.Arrays;
 
 public class Stack<T> {
     private T[] stack = (T[]) new  Object[1];
-    private int count = 0;
+    private int count;
 
     public Stack() {
+        count = 0;
+    }
 
+    public Stack(T[] stack) {
+        this.stack = stack;
+        count = 0;
     }
 
     public void push(T ob) {
-        if(count == stack.length) throw new StackFullException("The Stack is full " + count);
+        int val = count + 1;
+        if(count == stack.length) throw new StackFullException("The Stack is full. Push: " + val);
         stack[count++] = ob;
     }
 
     public T peek() {
         int val = count - 1;
-        return stack[val];
+        if(stack.length != 1) {
+            return stack[val];
+        }
+        return null;
     }
 
     public void pop() {
         count--;
+        if(count == -1) throw new StackEmptyException("der Stack ist empty");
         stack[count] = null;
     }
 }
