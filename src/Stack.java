b3d75a361304f2c46b0dad56f9f928e7bf1b001f
Christoph Pasterniak <cpasterniak@student.tgm.ac.at>
2024-02-12 09:40:36 +0100

    first implementation

---
+++ src/Stack.java
@@ -0,0 +1,19 @@
+public class Stack<T> {
+    private T[] stack = (T[]) new  Object[10];
+    private int count = stack.length;
+
+    public Stack() {
+
+    }
+    public void push() {
+
+    }
+
+    public T peek() {
+
+    }
+
+    public void pop() {
+
+    }
+}
